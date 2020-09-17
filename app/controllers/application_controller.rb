9b37421c6f72a283d4b4206c365b6c4311c02376
mohammadumar28 <mohammadumar28@gmail.com>
2020-09-17 15:19:31 +0530

    Complete Styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     def new_user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     end
 
     def create
+        @post = current_user.posts.build(post_params)
+
+        if @post.save
+            redirect_to root_path
+        end
     end
 
     def new
